49815caca52d930d4b98891e26ae7f0ccfafddc4
Matt Raible <matt.raible@okta.com>
2021-06-21 16:21:44 -0600

    Complete Material example

--- src/code/ng-demo-material/src/app/app.component.spec.ts
+++ src/code/ng-demo-material/src/app/app.component.spec.ts
@@ -1,16 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent],
+      imports: [RouterTestingModule, MatListModule, MatToolbarModule]
     }).compileComponents();
   });
 
@@ -30,6 +28,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ng-demo!');
+    expect(compiled.querySelector('mat-toolbar').textContent).toContain('Welcome to ng-demo!');
   });
 });
