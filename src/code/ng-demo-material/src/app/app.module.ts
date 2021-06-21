c6c9048bbf65b080a0c7908b8db3afcf9a881a7c
Matt Raible <matt.raible@okta.com>
2021-06-21 15:27:45 -0600

    Add Material Example

---
+++ src/code/ng-demo-material/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { EditComponent } from './edit/edit.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SearchComponent,
+    EditComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
