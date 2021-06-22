810ead8384658f35c96ced332455335daa6106e4
Matt Raible <matt.raible@okta.com>
2021-06-22 08:46:10 -0600

    Fix Okta tests

--- src/code/ng-demo-okta/src/app/app.component.spec.ts
+++ src/code/ng-demo-okta/src/app/app.component.spec.ts
@@ -39,6 +39,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ng-demo app is running!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ng-demo!');
   });
 });
