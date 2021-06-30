31ade0a85877ac806839ccb37b0ec798a81189d3
Matt Raible <matt.raible@okta.com>
2021-06-30 01:19:45 -0600

    Fix Barebones Angular + Spring Boot test

--- src/code/angular-spring-boot/notes/src/app/app.component.spec.ts
+++ src/code/angular-spring-boot/notes/src/app/app.component.spec.ts
@@ -38,7 +38,7 @@ describe('AppComponent', () => {
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('notes app is running!');
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('notes app is running!');
   });
 });
