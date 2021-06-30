13b89419575e0312d112ac1110e6a49c408b4b3b
Matt Raible <matt.raible@okta.com>
2021-06-30 01:58:06 -0600

    Angular 12.1 and other fixes

--- src/code/ng-demo-bootstrap/src/app/app.component.spec.ts
+++ src/code/ng-demo-bootstrap/src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ng-demo!');
+    expect(compiled.querySelector('.navbar').textContent).toContain('Welcome to ng-demo!');
   });
 });
