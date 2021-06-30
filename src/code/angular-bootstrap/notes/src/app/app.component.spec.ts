b5014bf5872e1145acc0f97af3da12042de3f5ae
Matt Raible <matt.raible@okta.com>
2021-06-30 01:31:53 -0600

    Fix tests

--- src/code/angular-bootstrap/notes/src/app/app.component.spec.ts
+++ src/code/angular-bootstrap/notes/src/app/app.component.spec.ts
@@ -25,20 +25,20 @@ describe('AppComponent', () => {
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
+    const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'notes'`, () => {
+  it(`should have as title 'Notes'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('notes');
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('Notes');
   });
 
-  it('should render title', () => {
+  it('should render Login button', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('notes app is running!');
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('ul').textContent).toContain('Login');
   });
 });
--- src/code/angular-spring-boot/notes/src/app/app.component.spec.ts
+++ src/code/angular-spring-boot/notes/src/app/app.component.spec.ts
@@ -25,13 +25,13 @@ describe('AppComponent', () => {
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
+    const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it(`should have as title 'notes'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
+    const app = fixture.componentInstance;
     expect(app.title).toEqual('notes');
   });
 
