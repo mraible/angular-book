89b6fb629971aee47d67f99220cde72743c83351
Matt Raible <matt.raible@okta.com>
2021-06-29 23:54:32 -0600

    Add MockSecurityConfiguration

--- src/code/angular-docker/notes-api/src/test/kotlin/com/okta/developer/notes/DemoApplicationTests.kt
+++ src/code/angular-docker/notes-api/src/test/kotlin/com/okta/developer/notes/DemoApplicationTests.kt
@@ -3,7 +3,7 @@ package com.okta.developer.notes
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
+@SpringBootTest(classes = [DemoApplication::class, MockSecurityConfiguration::class])
 class DemoApplicationTests {
 
 	@Test
