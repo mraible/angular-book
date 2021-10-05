22d75a2ec08c50be1e1e3b19f11007f7d1c50402
Matt Raible <matt.raible@okta.com>
2021-10-04 21:56:45 -0600

    Updates after QA

--- src/code/ng-demo-auth0/cypress/support/commands.ts
+++ src/code/ng-demo-auth0/cypress/support/commands.ts
@@ -45,5 +45,5 @@
 Cypress.Commands.add('signIn', (username, password) => {
   cy.get('input[name=username]').type(username);
   cy.get('input[name=password]').type(password);
-  cy.get('button[type=submit]').click();
+  cy.get('button[type=submit]').first().click();
 })
