8903cba99c19fa74943ba5fa0bd3121c153fe6c4
Matt Raible <matt@raibledesigns.com>
2023-04-15 22:35:28 -0600

    Remove JwtDecoder and related config

--- src/code/angular-docker/notes-api/src/main/kotlin/com/okta/developer/notes/SecurityConfiguration.kt
+++ src/code/angular-docker/notes-api/src/main/kotlin/com/okta/developer/notes/SecurityConfiguration.kt
@@ -1,15 +1,9 @@
 package com.okta.developer.notes
 
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.Customizer.withDefaults
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator
-import org.springframework.security.oauth2.core.OAuth2Error
-import org.springframework.security.oauth2.core.OAuth2TokenValidator
-import org.springframework.security.oauth2.core.OAuth2TokenValidatorResult
-import org.springframework.security.oauth2.jwt.*
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository
@@ -49,36 +43,13 @@ class SecurityConfiguration {
             headers.frameOptions { frameOptions -> frameOptions.sameOrigin() }
             headers.referrerPolicy { referrer -> referrer.policy(ReferrerPolicyHeaderWriter.ReferrerPolicy.STRICT_ORIGIN_WHEN_CROSS_ORIGIN) }
             headers.permissionsPolicy { permissions ->
-                permissions.policy("camera=(), fullscreen=(self), geolocation=(), gyroscope=(), " +
-                        "magnetometer=(), microphone=(), midi=(), payment=(), sync-xhr=()")
+                permissions.policy(
+                    "camera=(), fullscreen=(self), geolocation=(), gyroscope=(), " +
+                        "magnetometer=(), microphone=(), midi=(), payment=(), sync-xhr=()"
+                )
             }
         }
 
         return http.build()
-    }
-
-    @Value("\${okta.oauth2.audience}")
-    private val audience: String? = null
-
-    @Value("\${okta.oauth2.issuer}")
-    private val issuer: String? = null
-
-    @Bean
-    fun jwtDecoder(): JwtDecoder? {
-        val jwtDecoder = JwtDecoders.fromOidcIssuerLocation<NimbusJwtDecoder>(issuer)
-        val audienceValidator: OAuth2TokenValidator<Jwt> = AudienceValidator(audience)
-        val withIssuer: OAuth2TokenValidator<Jwt> = JwtValidators.createDefaultWithIssuer(issuer)
-        val withAudience: OAuth2TokenValidator<Jwt> = DelegatingOAuth2TokenValidator(withIssuer, audienceValidator)
-        jwtDecoder.setJwtValidator(withAudience)
-        return jwtDecoder
-    }
-}
-
-internal class AudienceValidator(private val audience: String?) : OAuth2TokenValidator<Jwt> {
-    override fun validate(jwt: Jwt): OAuth2TokenValidatorResult {
-        val error = OAuth2Error("invalid_token", "The required audience is missing", null)
-        return if (jwt.audience.contains(audience)) {
-            OAuth2TokenValidatorResult.success()
-        } else OAuth2TokenValidatorResult.failure(error)
     }
 }
