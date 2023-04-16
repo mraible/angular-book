efdf5c6dca66f8ac47cd004ffc65e2b69b2f30e1
Matt Raible <matt@raibledesigns.com>
2023-04-16 16:08:43 -0600

    Remove comment

--- watch-site.js
+++ watch-site.js
@@ -13,13 +13,6 @@ const watcher = chokidar.watch([
     /(^|[\/\\])\../, // ignore dotfiles
   ],
   persistent: true,
-  // avoid issues with Intellij IDEA temporary files (used to automatically save changes)
-  // [Error: ENOENT: no such file or directory, lstat '/path/to/.../file.adoc~' in /path/to/... (ref: antora-local-dev <worktree> | path: modules/...)] {
-  //   errno: -2,
-  //   code: 'ENOENT',
-  //   syscall: 'lstat',
-  //   path: '/path/to/.../file.adoc~'
-  // }
   awaitWriteFinish: {
     stabilityThreshold: 100,
     pollInterval: 50
