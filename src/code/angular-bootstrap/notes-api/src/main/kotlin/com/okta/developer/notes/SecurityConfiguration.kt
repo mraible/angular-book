81c13a8487b57633f78e11136d063c51a997a399
Matt Raible <matt@raibledesigns.com>
2023-04-17 22:01:35 -0600

    Remove audience validator

--- src/code/angular-bootstrap/notes-api/src/main/kotlin/com/okta/developer/notes/SecurityConfiguration.kt
+++ src/code/angular-bootstrap/notes-api/src/main/kotlin/com/okta/developer/notes/SecurityConfiguration.kt
@@ -1,17 +1,10 @@
 package com.okta.developer.notes
 
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.Customizer.withDefaults
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator
-import org.springframework.security.oauth2.core.OAuth2Error
-import org.springframework.security.oauth2.core.OAuth2TokenValidator
-import org.springframework.security.oauth2.core.OAuth2TokenValidatorResult
-import org.springframework.security.oauth2.jwt.*
 import org.springframework.security.web.SecurityFilterChain
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository
 import org.springframework.security.web.util.matcher.RequestMatcher
 import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.CorsConfigurationSource
@@ -34,9 +27,6 @@ class SecurityConfiguration {
         http.requiresChannel().requestMatchers(RequestMatcher { r ->
             r.getHeader("X-Forwarded-Proto") != null
         }).requiresSecure()
-
-        http.csrf()
-            .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
 
         http.headers()
             .contentSecurityPolicy("script-src 'self'; report-to /csp-report-endpoint/")
